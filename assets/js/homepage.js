ae1378ef497253e26c005a7809f47302961f25ae
Salman Ghouse <ghousemu@sheridancollege.ca>
2021-11-16 20:39:56 -0500

    added list repos and issues data

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,14 +1,75 @@
-var getUserRepos = function() {
-    console.log("function was called");
-    var apiUrl = "";
+let user = "";
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
 
-    fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+
+var getUserRepos = function(user) {
+    console.log("function was called");
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
-            console.log(data);
-        })
+            displayRepos(data, user);
+        });
     });
-    console.log("outside")
-    console.log(response);
 }
 
-getUserRepos();
+
+
+var displayRepos = function(repos, searchTerm) {
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+    for (var i = 0; i < repos.length; i++) {
+        // format repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+        // create a container for each repo
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+        // create a span element to hold repository name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+        // create a status element
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+
+        // check if current repo has issues or not
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML =
+                "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+        } else {
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        }
+
+        // append to container
+        repoEl.appendChild(statusEl);
+        // append to container
+        repoEl.appendChild(titleEl);
+
+        // append container to the dom
+        repoContainerEl.appendChild(repoEl);
+    }
+};
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+    console.log(event);
+};
+
+
+
+
+
+userFormEl.addEventListener("submit", formSubmitHandler);
